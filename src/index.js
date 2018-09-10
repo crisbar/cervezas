a5135682dbe81ebf55e02006d4d16d9b0d75fa6e
crisbar <crisbar1234@gmail.com>
2018-09-10 10:57:52 +0200

    feat(package.json): quitamos -w

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const cervezas = require("./cervezas.json");
 const uniqueRandomArray = require("unique-random-array");
 var _=require("lodash");
+//añadi
 module.exports = {
 	todas: _.sortBy(cervezas, ["nombre"]),
 	alazar: uniqueRandomArray(cervezas)
